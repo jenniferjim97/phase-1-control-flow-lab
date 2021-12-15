4cd8a198b4a5e6b811bee8434bd5db15f645a601
Jennifer Jimenez <jenniferjimenez90@gmail.com>
2021-12-15 15:45:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(dist){
+  let msg;
+  if(dist<=400){
+    msg = 'This one is on me!';
+  }
+  else if(2000 < dist && dist < 2500){
+    msg = 'I will gladly take your thirty bucks.';
+  }
+  else if(dist>2500){
+    msg = 'No can do.';
+  }
+  return msg
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+  let isNYC = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return isNYC
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let greeting;
+  switch(tip) {
+    case 'generous':
+   greeting = "Thank you so much.";
+   break;
+    case 'not as generous':
+      greeting = "Thank you.";
+      break;
+     default: 
+     greeting ="Bye.";
+  }
+return greeting
 }
